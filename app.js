099870135ad641e90b9cff330b5f24d8818a22d6
Ayush-Jain2 <ayushjainrksh@gmail.com>
2018-10-03 02:10:06 +0530

    4 new routes added

--- app.js
+++ app.js
@@ -6,7 +6,23 @@ app.set("view engine","ejs");
 app.use(express.static("assets"));
 
 app.get("/", function(req, res){
-       res.render("index.ejs");
+    res.render("index.ejs");
+});
+
+app.get("/left", function(req, res){
+    res.render("left-sidebar.ejs");
+});
+
+app.get("/right", function(req, res){
+    res.render("right-sidebar.ejs");
+});
+
+app.get("/no", function(req, res){
+    res.render("no-sidebar.ejs");
+});
+
+app.get("/contact", function(req, res){
+    res.render("contact.ejs");
 });
 
 app.listen(PORT, function(err){
